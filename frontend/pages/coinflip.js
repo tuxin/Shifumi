0f9030781d899d97bc71b10f881cb95b4be220b3
tuxin <dauge.damien@gmail.com>
2023-04-09 21:45:16 +0200

    Update coinflip.js

--- frontend/pages/coinflip.js
+++ frontend/pages/coinflip.js
@@ -37,8 +37,6 @@ export default function Home() {
       if(document.getElementById("inputvaluebet").value>ethers.utils.formatEther(arrayAllowToken)){
           document.getElementById("buttonconnect").innerHTML="Approve "+document.getElementById("inputaddressname").value
       }
-
-      
     }
     
   };
